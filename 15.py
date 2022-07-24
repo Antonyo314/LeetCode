d30f42f8a23219e3b07bf30a4e65706af244e51c
Antonyo314 <makarenko.antonf2c@gmail.com>
2022-07-24 23:27:12 +0300

    fix

--- 15.py
+++ 15.py
@@ -1,27 +1,19 @@
-from typing import List
-
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        nums = sorted(nums)
+        res = set()
 
-        positive = [i for i in nums if i >= 0]
-        negative = set([i for i in nums if i < 0])
+        temp_d = dict()
 
-        for i in range(len(positive)):
-            for j in range(i + 1, len(positive)):
-                if -(positive[i] + positive[j]) in negative:
-                    res.append(tuple(sorted([positive[i], positive[j], -(positive[i] + positive[j])])))
+        for i in range(len(nums)):
+            temp_d[nums[i]] = i
 
-        positive = set([i for i in nums if i >= 0])
-        negative = [i for i in nums if i < 0]
-
-        for i in range(len(negative)):
-            for j in range(i + 1, len(negative)):
-                if -(negative[i] + negative[j]) in positive:
-                    res.append(tuple(sorted([negative[i], negative[j], -(negative[i] + negative[j])])))
-
-        return list([list(i) for i in set(res)])
-
-
-print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
+        for i in range(len(nums)):
+            if nums[i] == nums[i - 1] and i != 0:
+                continue
+            for j in range(i + 1, len(nums)):
+                target = -(nums[i] + nums[j])
+                if target in temp_d and temp_d[target] > j:
+                    res.add((nums[i], nums[j], -(nums[i] + nums[j])))
+        res = [list(i) for i in res]
+        return res
