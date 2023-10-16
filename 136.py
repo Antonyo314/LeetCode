3431592671365e426941fd559f56f0423f26adac
Anton Makarenko <makarenko.antonf2c@gmail.com>
2023-10-16 22:09:41 +0300

    add problem 136 solution

---
+++ 136.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        s = set()
+        for i in nums:
+            if i in s:
+                s.remove(i)
+            else:
+                s.add(i)
+
+        return list(s)[0]
