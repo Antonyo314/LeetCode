2d63b8cdd94af251bf81406e38950967c1236554
Anton Makarenko <makarenko.antonf2c@gmail.com>
2023-10-17 14:09:24 +0300

    add problem 343 solution

---
+++ 343.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        b = n % 3
+        a = n - b
+        a //= 3
+
+        if n == 3:
+            return 2
+        if n == 2:
+            return 1
+        if b == 1:
+            return 3 ** (a - 1) * (b + 3)
+        if b == 0:
+            return 3 ** a
+        return 3 ** a * b
