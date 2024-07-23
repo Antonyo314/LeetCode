fc980391cbfbd7d7b579d2fbd6f20cef85a347e6
Anton Makarenko <makarenko.antonf2c@gmail.com>
2024-07-23 16:04:08 +0200

    add 260

---
+++ 260.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        s = set()
+
+        for i in nums:
+            if i in s:
+                s.remove(i)
+            else:
+                s.add(i)
+
+        return list(s)
