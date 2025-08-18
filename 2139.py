d12172fee76a144a5efd3c851e03bbe98f0feaa5
Anton Makarenko <makarenko.antonf2c@gmail.com>
2025-08-18 18:29:00 +0200

    add 2139.py

---
+++ 2139.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        res = 0
+        while target > 1:
+            if target % 2 == 0 and maxDoubles > 0:
+                maxDoubles -= 1
+                target //= 2
+                res += 1
+            elif maxDoubles == 0:
+                res += target - 1
+                return res
+            else:
+                target -= 1
+                res += 1
+
+        return res
+
+
+Solution().minMoves(target=10, maxDoubles=4)
